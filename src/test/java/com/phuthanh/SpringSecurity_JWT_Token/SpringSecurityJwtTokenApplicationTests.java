53024d588dbcbc538357dd8b94ef1ec6cb084ed7
LePhuThanh <lpthanh710@gmail.com>
2023-09-12 15:31:02 +0700

    init project & connect DB MýSQL

---
+++ src/test/java/com/phuthanh/SpringSecurity_JWT_Token/SpringSecurityJwtTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phuthanh.SpringSecurity_JWT_Token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
